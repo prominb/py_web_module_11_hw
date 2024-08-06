b9a55846c15e8eb553abde625cfd072d7e09f232
Roman_M <prominb@gmail.com>
2024-08-06 05:00:57 +0300

    e003 edit main add healthchecker

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from config.db import get_db
 from src.contacts.routers import router as router_contacts
 
 
 app = FastAPI()
 
 app.include_router(router_contacts, prefix="/contacts", tags=["contacts"])
+
+
+@app.get("/api/healthchecker")
+async def healthchecker(db: AsyncSession = Depends(get_db)):
+    try:
+        result = await db.execute(text("SELECT 1"))  #.fetchone()  # Welcome to FastAPI!
+        # RuntimeWarning: Enable tracemalloc to get the object allocation traceback
+        # 'coroutine' object has no attribute 'fetchone'
+        result = result.fetchone()
+        if result is None:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Database is not configured correctly")
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error connecting to the database")
